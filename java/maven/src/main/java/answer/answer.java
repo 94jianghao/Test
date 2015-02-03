1d38079538f169d148413ed636a35cb58f655c21
94jianghao <jh_mr@sina.com>
2015-02-03 17:07:24 +0800

    new project

--- java/maven/src/main/java/answer/answer.java
+++ java/maven/src/main/java/answer/answer.java
@@ -6,14 +6,21 @@ import AnswerGenerator.AnswerGenerator;
  * Created by jh_pc on 15-2-3.
  */
 public class answer {
-    public void say() {
+    public String say() {
 
         AnswerGenerator isgo=new AnswerGenerator();
         int random=isgo.return_randomnum();
-        int[] randoms={random/1000,random/100%10,random/10%10,random%10};
-        for(int i=0;i<randoms.length;i++)
+       String randoms =random+"";
+        System.out.println(randoms);
+        for(int i=0;i<randoms.length();i++)
         {
+             if(randoms.indexOf(randoms.substring(i,i+1))!=i&&randoms.indexOf(randoms.substring(i,i+1))!=-1)
+             {
+                 randoms=say();
+                 break;
+             }
 
         }
+        return  randoms;
     }
 }
--- java/maven/src/test/java/randomnum.java
+++ java/maven/src/test/java/randomnum.java
@@ -14,28 +14,70 @@ public class randomnum {
         * 测试类
         */
        public void should_number_Test(){
-           //new 一个产生随机数的类
-           AnswerGenerator answerGenerator = new AnswerGenerator();
-           //new 一个产生比较测试类 返回 一个String类型的结果
-           CompareNumber compareNumber = new CompareNumber();
-           String result= compareNumber.numTest(answerGenerator.return_randomnum(),-1);
-           System.out.println(result);
+           String result=result();
            assertThat(result).isEqualTo("4A0B");
-           assertThat(result).isEqualTo("0A4B");
+
+       }
+    @Test
+    public void should_number_Test5(){
+        String result=result();
            assertThat(result).isEqualTo("0A0B");
+
+
+    }
+    @Test
+    public void should_number_Test2(){
+        String result=result();
+
+           assertThat(result).isEqualTo("0A0B");
+
+
+
+    }
+    @Test
+    public void should_number_Test3(){
+        String result=result();
+
            assertThat(result).isEqualTo("2A2B");
+
+
+
+    }
+    @Test
+    public void should_number_Test4(){
+        String result=result();
+
            assertThat(result).isEqualTo("2A1B");
 
 
-       }
+    }
 
+
+
+    @Test
     public  void should_number_Test1(){
         //new 一个产生随机数的类
         AnswerGenerator answerGenerator = new AnswerGenerator();
         //new 一个比较类
-        answer answer = new answer();
-        answer.say();
+        answer answer  = new answer();
+        String result=answer.say();
+        String result1=answer.say();
+        String result2=answer.say();
+        System.out.println("不重复结果1："+result);
+        System.out.println("不重复结果2："+result1);
+        System.out.println("不重复结果3："+result2);
+        assertThat(result == result1 || result == result2 || result2 == result1).isEqualTo(false);
+
 
     }
 
+    public  String result(){
+        //new 一个产生随机数的类
+        AnswerGenerator answerGenerator = new AnswerGenerator();
+        //new 一个产生比较测试类 返回 一个String类型的结果
+        CompareNumber compareNumber = new CompareNumber();
+        String result= compareNumber.numTest(answerGenerator.return_randomnum(),-1);
+        System.out.println("产生结果：+++"+result);
+        return  result;
+    }
 }
